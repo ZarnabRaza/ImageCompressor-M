634ea0065c973d63f97fdec9b9b3e4dc93ec8e69
Zarnab Raza <141522632+ZarnabRaza@users.noreply.github.com>
2024-07-19 13:45:19 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import ImageCompressor from "./components/ImageCompressor";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => <ImageCompressor />;
+
+export default App;
